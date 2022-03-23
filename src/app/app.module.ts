614d6f0a612e63a717d4740c3313f918229b5ff7
Timofei Torshin <timtorshin@gmail.com>
2022-03-23 12:11:18 +0300

    feat: created pipe for number formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { AttributeDirective } from './attribute.directive';
+import { NumberFormatPipe } from './number-format.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    AttributeDirective
+    AttributeDirective,
+    NumberFormatPipe
   ],
   imports: [
     BrowserModule
